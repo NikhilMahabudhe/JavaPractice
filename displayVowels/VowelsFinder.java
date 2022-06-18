82f2035c8821a87867228bce92ce55f8f08f2a89
Niks <106048768+NikhilMahabudhe@users.noreply.github.com>
2022-06-18 14:13:48 +0530

    Add files via upload

---
+++ displayVowels/VowelsFinder.java
@@ -0,0 +1,13 @@
+package displayVowels;
+
+import java.util.Scanner;
+
+public class VowelsFinder {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("please enter your string:");
+    String str = sc.nextLine();
+    int len = str.length();
+    for( int i=0; i=<len-1;i++){
+
+    }
+}
